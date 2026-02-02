de326a899f3f99dd0189b9e103c72a775273655b
PaxoJW <184737673+PaxoJW@users.noreply.github.com>
2026-02-02 08:40:33 +0100

    Made other 4 functions
    I need to test all of them so it might be all wrong.

--- linkedList.js
+++ linkedList.js
@@ -1,7 +1,10 @@
 function LinkedList() {
     let list = [];
     function append(value) {
-        if (list.nextNode === null) {
+        if (list.length === 0) {
+            list = Node(value);
+        }
+        else if (list.nextNode === null) {
             list.nextNode = Node(value);
         }
         else list.nextNode.append(value);
@@ -13,8 +16,59 @@ function LinkedList() {
         preNode.nextNode(list);
     }
 
-    return append, prepend;
+    let size = 0;
+    function size() {
+        if (list.nextNode === null) {
+            size += 1;
+            return size;
+        }
+        else {
+            size += 1;
+            const nextList = list.nextNode;
+            return nextList.size();
+        }
+    }
+
+    function head() {
+        if (list.length === 0) {
+            return undefined;
+        } else {
+            return value;
+        }
+    }
+
+    function tail() {
+        if (list.length === 0) {
+            return undefined;
+        } else if (list.nextNode === null) {
+            return value;
+        } else {
+            const nextList= list.nextNode;
+            nextList.tail();
+        }
+    }
+
+    function at(index) {
+        if (index > list.length - 1) {
+            return undefined;
+        } else {
+            for (let i = 0; i < index; i ++){
+                if (i === index) {
+                    return value;
+                } else {
+                    const nextList = list.nextNode;
+                    nextList.at(i);
+                }
+            }
+        }
+    }
+
+
+
+
+    return append, prepend, size, head, tail, at, pop, contains, findIndex, toString;
 }
+
 function Node(v) {
     const value = v;
     const nextNode = null;
